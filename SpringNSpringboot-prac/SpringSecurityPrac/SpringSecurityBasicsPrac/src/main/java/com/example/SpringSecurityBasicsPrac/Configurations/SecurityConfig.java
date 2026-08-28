2da18ca12e185a0843214980c97fea0e6b54459d
Daniel Jayakaran <jdanieljayakaran02@gmail.com>
2026-08-28 22:17:41 +0530

    Practiced multiple-users Authentication

--- SpringNSpringboot-prac/SpringSecurityPrac/SpringSecurityBasicsPrac/src/main/java/com/example/SpringSecurityBasicsPrac/Configurations/SecurityConfig.java
+++ SpringNSpringboot-prac/SpringSecurityPrac/SpringSecurityBasicsPrac/src/main/java/com/example/SpringSecurityBasicsPrac/Configurations/SecurityConfig.java
@@ -7,10 +7,16 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -28,5 +34,40 @@ public class SecurityConfig {
                                 .sessionManagement(session ->
                                                       session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                                     .build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        List<UserDetails> users = new ArrayList<>();
+
+        UserDetails user1 = User.withDefaultPasswordEncoder()
+                                .username("Suman")
+                                    .password("212")
+                                        .roles("USER")
+                                            .build();
+
+        UserDetails user2 = User.withDefaultPasswordEncoder()
+                .username("Rajest")
+                .password("12")
+                .roles("USER")
+                .build();
+
+        UserDetails user3 = User.withDefaultPasswordEncoder()
+                .username("Kuja")
+                .password("111")
+                .roles("USER")
+                .build();
+
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+
+//      Collections
+        return new InMemoryUserDetailsManager(users);
+
+//        Var Args Methods
+//        return new InMemoryUserDetailsManager(user1, user2, user3);
     }
 }
