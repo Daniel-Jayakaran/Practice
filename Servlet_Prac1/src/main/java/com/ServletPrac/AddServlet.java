0c84cee1987a5739832f316c5ab3d7334bb4058c
Daniel Jayakaran <jdanieljayakaran02@gmail.com>
2026-09-03 23:52:06 +0530

    Servlet Basics setup and web.xml config Prac

---
+++ Servlet_Prac1/src/main/java/com/ServletPrac/AddServlet.java
@@ -0,0 +1,24 @@
+package com.ServletPrac;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.http.HttpResponse;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int a = Integer.parseInt(req.getParameter("num1"));
+		int b = Integer.parseInt(req.getParameter("num2"));
+		
+		int c = a + b;
+		
+		PrintWriter out = res.getWriter();
+		
+		out.println("The Result is: " + c);
+	}
+
+}
